c35d1c7d0430ef7f892d90dd7ff01a80c5b54333
realkendo <kennethistifanus1@gmail.com>
2024-10-23 15:14:15 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Object Oriented Programming with PHP</title>
+  <style>
+    body{
+      display: grid;
+      align-items: center;
+      justify-content: center;
+    }
+    h1{
+      color: red;
+    }
+  </style>
+</head>
+<body>
+    <h1>Welcome to Object Orientation with PHP</h1>
+</body>
+</html>
